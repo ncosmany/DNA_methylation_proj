bec8b49c9ab7de47f7b199bf1ddf8799051ea469
Nicole Cosmany <153880342+ncosmany@users.noreply.github.com>
2024-12-09 11:59:31 -0700

    Update rehead.sh
    rehead as an array

--- rehead.sh
+++ rehead.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+#SBATCH --mem=10GB
+#SBATCH --time=22:00:00 
 
 module load GCC/11.3.0
 module load BCFtools/1.18
 
-
 #rehead so NA0001 replaced with sample names
-#do this after running merge, all files from merge will go in order of their folder so just use that same list
 
-#rehead with sample names 
-bcftools reheader -s file_list.txt ../outputs/merged_files.vcf.gz > ../outputs/reheaded_merged.vcf.gz
+for file in ../outputs/finalvcfs/*.vcf.gz; do
+   sample_name=$(basename $file .vcf.gz)
+   echo $sample_name > currsample.txt 
+   bcftools reheader -s currsample.txt -o ../outputs/reheaded/$sample_name.vcf.gz $file
+done
